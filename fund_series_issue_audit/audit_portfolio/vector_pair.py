fix: further improve inner product calculation with raw data preservation

fund_series_issue_audit/audit_portfolio/vector_pair.py
@@ -16,12 +16,13 @@ class VectorPair:
 
     def get_comparison(self, option_delta=True):
         if self.comparison is None:
-            comparison = self.pv_i.portfolio.merge(self.pv_j.portfolio, how='outer', left_index=True, right_index=True, suffixes=(f'_{self.pv_i.fund_code}', f'_{self.pv_j.fund_code}'))
+            _comparison = self.pv_i.portfolio.merge(self.pv_j.portfolio, how='outer', left_index=True, right_index=True, suffixes=(f'_{self.pv_i.fund_code}', f'_{self.pv_j.fund_code}'))
             if self.pv_i.fund_code == self.pv_j.fund_code:
-                comparison = self.pv_i.portfolio.merge(self.pv_j.portfolio, how='outer', left_index=True, right_index=True, suffixes=(f'_{self.pv_i.fund_code}({self.pv_i.date_ref})', f'_{self.pv_j.fund_code}({self.pv_j.date_ref})'))            
+                _comparison = self.pv_i.portfolio.merge(self.pv_j.portfolio, how='outer', left_index=True, right_index=True, suffixes=(f'_{self.pv_i.fund_code}({self.pv_i.date_ref})', f'_{self.pv_j.fund_code}({self.pv_j.date_ref})'))            
             if option_delta:
-                comparison['delta'] = comparison.iloc[:,-3] - comparison.iloc[:,-1]
-            comparison = comparison.fillna('-')
+                _comparison['delta'] = _comparison.iloc[:,-3] - _comparison.iloc[:,-1]
+            self._comparison = _comparison
+            comparison = _comparison.fillna('-')
             self.comparison = comparison
         return self.comparison
 
@@ -33,18 +34,11 @@ class VectorPair:
 
     def get_vectors(self):
         if self.coeffs_i is None and self.coeffs_j is None:
-            comparison = self.get_comparison()
-            # 첫 번째 컬럼과 세 번째 컬럼을 추출 (delta 컬럼이 추가되었을 경우 고려)
-            col_i = 0 if 'delta' in comparison.columns else 0
-            col_j = 2 if 'delta' in comparison.columns else 1
-            
-            # '-' 문자를 0으로 변환하고 숫자형으로 변환
-            self.vector_i = comparison.iloc[:, col_i:col_i+1].replace('-', 0).astype(float)
-            self.vector_j = comparison.iloc[:, col_j:col_j+1].replace('-', 0).astype(float)
-            
-            # 벡터 추출
-            self.coeffs_i = np.array(self.vector_i.iloc[:, 0], dtype=float)
-            self.coeffs_j = np.array(self.vector_j.iloc[:, 0], dtype=float)
+            self.get_comparison()
+            self.vector_i = self._comparison.iloc[:, 1:2].fillna(0)
+            self.vector_j = self._comparison.iloc[:, 3:4].fillna(0)
+            self.coeffs_i = np.array(self.vector_i.iloc[: ,-1])
+            self.coeffs_j = np.array(self.vector_j.iloc[: ,-1])
         return self.coeffs_i, self.coeffs_j
 
     def get_inner_product(self):